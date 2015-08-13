4919305cb5567f2d1d11befb281998f99248259f
小室敦 <komuroatsushi@komuroatsushi-no-MacBook-Pro.local>
2015-08-14 02:11:51 +0900

    カメラセッションのセットアップ

--- DevTest/ViewController.swift
+++ DevTest/ViewController.swift
@@ -1,25 +1,59 @@
 //
 //  ViewController.swift
-//  DevTest
+//  sousa
 //
-//  Created by 小室敦 on 2015/08/11.
-//  Copyright © 2015年 小室敦. All rights reserved.
+//  Created by Misato Morino on 2015/08/13.
+//  Copyright (c) 2015年 Atsushi Komuro. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    //セッション
+    var CapSession : AVCaptureSession!
+    //デバイス
+    var CapDevice : AVCaptureDevice!
+    //画像アウト
+    var ImageOut : AVCaptureStillImageOutput!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        CapSession = AVCaptureSession()
+        let devices = AVCaptureDevice.devices()
+        for device in devices{
+            if(device.position == AVCaptureDevicePosition.Back){
+                CapDevice = device as! AVCaptureDevice
+            }
+        }
+        
+        //VideoInputを取得、セッションに追加
+        do{
+            let videoInput = try AVCaptureDeviceInput(device: CapDevice)
+            CapSession.addInput(videoInput)
+        }catch{
+            
+        }
+        
+        //出力先を生成
+        ImageOut = AVCaptureStillImageOutput()
+        
+        //セッションに追加
+        CapSession.addOutput(ImageOut)
+        
+        //画像を表示するレイヤーを生成
+        let videoLayer : AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: CapSession!) as AVCaptureVideoPreviewLayer
+        
+        videoLayer.frame = self.view.bounds
+        videoLayer.videoGravity = AVLayerVideoGravityResizeAspect
+        
+        // Viewに追加
+        self.view.layer.addSublayer(videoLayer)
+        
+        //セッション開始
+        CapSession.startRunning()
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
