deacd77351549b28b3ae9dac58bb36aec10af869
小室敦 <komuroatsushi@komuroatsushi-no-MacBook-Pro.local>
2015-08-14 02:10:10 +0900

    カメラセッションのセットアップ

---
+++ DevTestTests/DevTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  DevTestTests.swift
+//  DevTestTests
+//
+//  Created by 小室敦 on 2015/08/11.
+//  Copyright © 2015年 小室敦. All rights reserved.
+//
+
+import XCTest
+@testable import DevTest
+
+class DevTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
